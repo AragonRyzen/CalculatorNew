00b191212970c7cdf7f637bcb22e2c7128910552
Aragon <faragon12355@gmail.com>
2020-02-07 20:35:36 +0300

    Version 1.0

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);//Используется для занесения данных из консоли
+        System.out.println("Введите первую цифру");
+        double x1, x2, y;
+        int v;
+        x1 = in.nextDouble();//получение значения для первой переменной
+        System.out.println("Введите вторую цифру");
+        x2 = in.nextDouble();//получение значения для второй переменной
+        System.out.println("Что необходимо сделать?\n1)Сложить\n2)Отнять\n");
+        v = in.nextInt();
+        if (v == 1) {y = x1+x2; System.out.println("Результат сложения = "+ y);}
+        if (v == 2) {y = x1-x2; System.out.println("Результат вычитания = "+ y);}
+        in.close();
+    }
+}
